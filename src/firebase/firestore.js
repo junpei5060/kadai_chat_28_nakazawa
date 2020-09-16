2382dfb95389358db52690abd86b28e60d4ea2e0
junpei5060 <66504532+junpei5060@users.noreply.github.com>
2020-09-16 20:10:51 +0900

    Update firestore.js

--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -2,13 +2,7 @@ import Firebase from 'firebase'
 import 'firebase/firestore'
 
 const config = {
-    apiKey: "AIzaSyA365DFhVAAH_WLASehC8uBYi6R25xkfB0",
-    authDomain: "chat-app-1715d.firebaseapp.com",
-    databaseURL: "https://chat-app-1715d.firebaseio.com",
-    projectId: "chat-app-1715d",
-    storageBucket: "chat-app-1715d.appspot.com",
-    messagingSenderId: "239344260010",
-    appId: "1:239344260010:web:bfdae543d4531602dbd1d3"
+    
 }
 
 const firebaseApp = Firebase.initializeApp(config, 'exercise-vue')
